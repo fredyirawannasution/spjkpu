3cc145aa4c0d27106e06884be9062ff1ef8933ff
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-25 20:25:32 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Ganti nilai ini dengan URL Web App Google Apps Script Anda.
-// Format contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxx/exec
-window.SPJ_APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqnNwahGSVqzEUqD0BkBySxji_35sCrjL5jIEEiaaWhsXD0JowuROlbWa7AzvJJWF1Rg/exec',
-  APP_NAME: 'SPJ KPU Labuhanbatu Utara'
-};
