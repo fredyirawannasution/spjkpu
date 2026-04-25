78157a546540d465a2c30e68ae80fb9447e97bfd
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-25 20:26:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Ganti nilai ini dengan URL Web App Google Apps Script Anda.
+// Format contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxx/exec
+window.SPJ_APP_CONFIG = {
+  APPS_SCRIPT_URL: 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  APP_NAME: 'SPJ KPU Labuhanbatu Utara'
+};
